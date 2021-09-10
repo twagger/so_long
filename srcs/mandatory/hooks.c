29e3b1e76d3eef478abe820df0f88417656b6adc
twagner <twagner@student.42.fr>
2021-09-10 10:08:41 +0200

    switch keyboard layout

--- srcs/mandatory/hooks.c
+++ srcs/mandatory/hooks.c
@@ -6,24 +6,24 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 11:01:18 by twagner           #+#    #+#             */
-/*   Updated: 2021/09/03 10:45:57 by twagner          ###   ########.fr       */
+/*   Updated: 2021/09/10 10:07:50 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-/*
 #define ESC 53
 #define W 13
 #define S 1
 #define A 0
 #define D 2
-#define KEYMAP "321----------0"*/
+#define KEYMAP "321----------0"
+/*
 #define ESC 65307
 #define W 122
 #define S 115
 #define A 113
 #define D 100
-#define KEYMAP "----------------------------------------------------------------------------------------------------1------------3-2------0--------"
+#define KEYMAP "----------------------------------------------------------------------------------------------------1------------3-2------0--------"*/
 
 
 int	ft_handle_key(int key, void *param)
@@ -41,7 +41,6 @@ int	ft_handle_key(int key, void *param)
 			exit(0);
 		}
 	}
-	ft_putnbr_fd(key, 1);
 	ft_putstr_fd("Current move count : ", 1);
 	ft_putnbr_fd(((t_param *)param)->nb_moves, 1);
 	ft_putchar_fd('\n', 1);
