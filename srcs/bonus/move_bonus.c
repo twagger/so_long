614c23d98cd312bcc0f287f72fed4abfded8298a
twagner <twagner@student.42.fr>
2021-09-12 11:35:12 +0200

    fix : error on exit tile

--- srcs/bonus/move_bonus.c
+++ srcs/bonus/move_bonus.c
@@ -6,17 +6,36 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 15:45:51 by twagner           #+#    #+#             */
-/*   Updated: 2021/09/12 11:11:47 by twagner          ###   ########.fr       */
+/*   Updated: 2021/09/12 11:32:55 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
-void	ft_register_move(t_sprite p, int move, t_param *param, char next_tile)
+void	ft_update_map_array(t_sprite p, t_param *param, char next_tile)
 {
 	int	on_exit;
 
 	on_exit = 0;
+	if (next_tile == '1')
+	{
+		param->move.from_exit = 0;
+		return ;
+	}
+	if (param->is_on_exit == 1)
+		param->map->map[p.y][p.x] = 'E';
+	else
+		param->map->map[p.y][p.x] = '0';
+	param->move.from_exit = param->is_on_exit;
+	if (param->map->map[p.next_y][p.next_x] == 'E')
+		on_exit = 1;
+	param->is_on_exit = on_exit;
+	param->move.to_exit = on_exit;
+	param->map->map[p.next_y][p.next_x] = 'P';
+}
+
+void	ft_register_move(t_sprite p, int move, t_param *param, char next_tile)
+{
 	param->move.nb_move = NBMOVES;
 	param->move.x = p.x;
 	param->move.y = p.y;
@@ -26,19 +45,9 @@ void	ft_register_move(t_sprite p, int move, t_param *param, char next_tile)
 	param->move.img[0] = param->img[(move * 3) + 5];
 	param->move.img[1] = param->img[(move * 3) + 6];
 	param->move.img[2] = param->img[(move * 3) + 7];
-	if (next_tile == '1')
-		return ;
-	if (param->is_on_exit == 1)
-		param->map->map[p.y][p.x] = 'E';
-	else
-		param->map->map[p.y][p.x] = '0';
-	param->move.from_exit = param->is_on_exit;
-	if (param->map->map[p.next_y][p.next_x] == 'E')
-		on_exit = 1;
-	param->is_on_exit = on_exit;
-	param->move.to_exit = on_exit ;
-	param->map->map[p.next_y][p.next_x] = 'P';
-	++(param->curr_moves);
+	ft_update_map_array(p, param, next_tile);
+	if (next_tile != '1')
+		++(param->curr_moves);
 }
 
 void	ft_erase_move_zone(t_param *param)
