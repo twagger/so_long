bc37ae61b640bcdd62806616d21d5e9d26f88016
twagner <twagner@student.42.fr>
2021-09-20 09:04:03 +0200

    removing a useless unknown fonction on mac

--- srcs/mandatory/hooks.c
+++ srcs/mandatory/hooks.c
@@ -6,7 +6,7 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 11:01:18 by twagner           #+#    #+#             */
-/*   Updated: 2021/09/19 11:28:51 by twagner          ###   ########.fr       */
+/*   Updated: 2021/09/20 09:03:03 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,13 @@
 int	ft_handle_key(int key, void *param)
 {
 	if (key == K_QUIT)
-	{
-		mlx_loop_end(((t_param *)param)->mlx);
 		ft_free_mem(((t_param *)param)->map, (t_param *)param, \
 			((t_param *)param)->mlx, 0);
-	}
 	if (key == K_UP || key == K_DOWN || key == K_LEFT || key == K_RIGHT)
 	{
 		if (ft_mover(param, ft_getmove(key)) == 1)
-		{
-			mlx_loop_end(((t_param *)param)->mlx);
 			ft_free_mem(((t_param *)param)->map, (t_param *)param, \
 				((t_param *)param)->mlx, 0);
-		}
 	}
 	return (0);
 }
